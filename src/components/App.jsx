c628afd11350c1205198f5ad30b36372436e9ada
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-06-26 12:23:05 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import UserCards from './Profile/userCard'
 import user from './Profile/user.json';
@@ -19,3 +20,20 @@ const App = () => (
   );
   
   export default App;
+
+  // export const App = () => {
+    //   return (
+    //     <div
+    //       style={{
+    //         height: '100vh',
+    //         display: 'flex',
+    //         justifyContent: 'center',
+    //         alignItems: 'center',
+    //         fontSize: 40,
+    //         color: '#010101'
+    //       }}
+    //     >
+    //       React homework template
+    //     </div>
+    //   );
+    // };
